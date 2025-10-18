0e879a2191b3040871520dea49b40e0b3c5fdaf8
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2025-10-18 23:06:25 +0530

    Time: 1 ms (57.72%) | Memory: 19 MB (33.56%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff_map = {}
+        for index, value in enumerate(nums):
+            diff = target - value
+
+            if diff in diff_map:
+                return index, diff_map[diff]
+                
+            diff_map[value] = index
