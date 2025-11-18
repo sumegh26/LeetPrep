8fc66e9651e0eb9a1eb93f9bc7ef3686983c641f
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2025-11-19 00:16:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (24.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # bruteforce, target, nums
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if target == nums[i]+ nums[j]:
+        #             return [i,j]
+        # Hashmap
+        mapperr = {}
         for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if target == nums[i]+ nums[j]:
-                    return [i,j]
-        
+            comple_sum = target-nums[i]
+            if comple_sum in mapperr:
+                return [i,mapperr[comple_sum]]
+            mapperr[nums[i]]=i
