1f534c30984a3c1a010339a458c4466233c8a8ac
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2025-11-19 01:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (72.85%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,13 +1,22 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        result = []
-        for i in range(len(nums)):
-            cnt = 0
-            for j in range(len(nums)):
-                if nums[i]>nums[j]:
-                    cnt += 1
-            result.append(cnt)
-        return result
+        # result = []
+        # for i in range(len(nums)):
+        #     cnt = 0
+        #     for j in range(len(nums)):
+        #         if nums[i]>nums[j]:
+        #             cnt += 1
+        #     result.append(cnt)
+        # return result
+        sortedNums = sorted(nums)
+        mappr = {}
+        for index,value in enumerate(sortedNums):
+            if value not in mappr:
+                mappr[value] = index
+        ret = []
+        for i in nums:
+            ret.append(mappr[i])
+        return ret
                 
             
 
