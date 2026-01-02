837e9116a036a95f5c99c2c9b2eba258c229b349
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2026-01-02 23:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (46.84%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,14 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # bruteforce, target, nums
-        # for i in range(0,len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if target == nums[i]+ nums[j]:
-        #             return [i,j]
-        # Hashmap
-        mapperr = {}
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mapp = {}
         for i in range(0,len(nums)):
-            comple_sum = target-nums[i]
-            if comple_sum in mapperr:
-                return [i,mapperr[comple_sum]]
-            mapperr[nums[i]]=i
+            diff = target-nums[i]
+            if diff in mapp:
+                return [i,mapp[diff]]
+            mapp[nums[i]]=i
+        
