b8a7c0475711759725c85f3ec0b0d48909c7925a
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2026-01-08 21:38:13 +0530

    Time: 1 ms (61.94%) | Memory: 13.4 MB (16.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,10 +5,15 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
         mapp = {}
         for i in range(len(nums)):
             diff = target-nums[i]
             if diff in mapp:
                 return [i,mapp[diff]]
-            mapp[nums[i]]=i
-         
+            mapp[nums[i]] = i
+            
