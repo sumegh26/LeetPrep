f75ca7db5da4002ac7e3bd7d528f6b6cd089d1ed
sumegh thale <40857938+sumegh26@users.noreply.github.com>
2026-01-09 19:49:36 +0530

    Time: 2129 ms (20.20%) | Memory: 13.4 MB (16.38%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -9,3 +9,11 @@ class Solution(object):
             for j in range(i+1,len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i,j]
+# A + B = C
+# A - C = B
+        mapper = {}
+        for index,value in enumerate(nums):
+            diff = target - value
+            if diff in mapper:
+                return [index, mapper[diff]]
+            mapper[value] = index
